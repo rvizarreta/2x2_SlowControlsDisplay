ddd073f282abd29891ec26b4fa8e9930b1ecc307
ArgonCube <rvizarreta14@gmail.com>
2023-08-05 14:04:06 -0500

    test notebooks done

--- MyApp/CLASSES/UNIT_library.py
+++ MyApp/CLASSES/UNIT_library.py
@@ -23,41 +23,18 @@ class UNIT():
         Create InfluxDB client
         '''
         conf = ConfigParser()
-        conf.read("../CONFIG/config.ini")
+        conf.read("CONFIG/config.ini")
         db = conf["DATABASE"]
         client = InfluxDBClient('localhost', db.get('PORT'), self.unit) 
-        db_name = self.module + "_" + self.unit
+        if self.module == None:
+            db_name = self.unit
+        else:
+            db_name = self.module + "_" + self.unit
         client.create_database(db_name)
         client.switch_database(db_name)
         return client 
     
-    def JSON_setup(self, measurement, channel_name, status, fields):
-            '''
-            Inputs:         - Measurement (i.e. light)
-                            - Channel name (i.e. VGA_12_POS)
-                            - Status (i.e. OFF)
-                            - Fields (i.e. Voltage & current)
 
-            Outputs:        - JSON file ready to be added to InfluxDB
-
-            Description:    Provides new timestamp ready to be added to InfluxDB
-            '''
-            json_payload = []
-            data = {
-                # Table name
-                "measurement" : measurement, 
-                # Organization tags
-                "tags" : { 
-                    "channel_name" : channel_name,
-                    "status" : status
-                },
-                # Time stamp
-                "time" : datetime.utcnow().strftime('%Y%m%d %H:%M:%S'),
-                # Data fields 
-                "fields" : dict(fields)
-            }
-            json_payload.append(data)
-            return json_payload
 
     
 
