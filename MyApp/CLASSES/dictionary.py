4e71f4ca77874f4086e3353be607b2d23af52ddc
ArgonCube <rvizarreta14@gmail.com>
2023-08-06 03:23:16 -0500

    Fast API implementation added

---
+++ MyApp/CLASSES/dictionary.py
@@ -0,0 +1,7 @@
+from CLASSES.MPOD_library import MPOD
+from CLASSES.GIZMO_library import GIZMO
+
+classes_dictionary = {
+    "MPOD" : MPOD,
+    "GIZMO" : GIZMO
+}
--- MyApp/main.py
+++ MyApp/main.py
@@ -1,10 +1,163 @@
+#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---
+# FAST API PACKAGES
+#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---
+# INTERNAL MANAGEMENT CLASSES
+#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---
+from CLASSES.MPOD_library import MPOD
+from CLASSES.MPOD_library import UNIT
+from CLASSES.dictionary import classes_dictionary
+import json, time
+
+#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---
+# GENERATING OBJECT MODELS
+#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---
+# Reading modules JSON file
+with open('CONFIG/modules_units.json', "r") as json_file:
+    moduleDB = json.load(json_file)
+
+# Reading other units JSON file
+with open('CONFIG/others_units.json', "r") as json_file:
+    othersDB = json.load(json_file)
+
+# Get list of units  attached to modules
+id = 0
+attached_units_dict = {}
+for module in moduleDB.keys():
+    unit_names = moduleDB[module].keys()
+    for unit in unit_names:
+        kind = moduleDB[module][unit]["class"]
+        object = classes_dictionary[kind]
+        attached_units_dict[id] = object(module, unit, moduleDB[module][unit])
+        id += 1
+
+id = 0
+others_dict = {}
+# Get list of units not attached to modules
+for unit in othersDB.keys():
+    kind = othersDB[unit]["class"]
+    object = classes_dictionary[kind]
+    others_dict[id] = object(None, unit, othersDB[unit])
+    id += 1
+    
+
+
+
+#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---
+# FAST API CONFIGURATION
+#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---
+# FastAPI handles JSON serialization and deserialization for us.
 app = FastAPI()
 
-# FastAPI handles JSON serialization and deserialization for us.
-# We can simply use built-in python and Pydantic types, in this case dict[int, Item].
+#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---
+# GET METHODS
+#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---
 @app.get("/")
-def index() -> Dict[str, Dict[int, Item]]:
-    return {"items": items}
+def index():
+    return {"message" : "Hello World!"}
+
+@app.get("/allmodules")
+def get_ModulesJSON():
+    '''
+    Return modules JSON file
+    '''
+    return moduleDB
+
+@app.get("/allothers")
+def get_other_modulesJSON():
+    '''
+    Return other units (i.e. Gizmo) JSON file
+    '''
+    return othersDB
+
+@app.get("/attached_units")
+def get_attached_units():
+    '''
+    Return all objects of units connected to modules
+    '''
+    return attached_units_dict
+
+@app.get("/attached_units/{unit_id}")
+def get_attached_unit_by_id(unit_id: int):
+    '''
+    Return object by id
+    '''
+    return attached_units_dict[unit_id]
+
+@app.get("/other_units")
+def get_other_units():
+    '''
+    Return all objects of units NOT connected to modules
+    '''
+    return others_dict
+
+@app.get("/other_units/{unit_id}")
+def get_others_by_id(unit_id: int):
+    '''
+    Return object by id
+    '''
+    return others_dict[unit_id]
+
+@app.get("/attached_units/{unit_id}/status")
+def get_attached_status_by_id(unit_id: int):
+    '''
+    Return unit status of measuring elements (i.e. {light, current, rtd})
+    '''
+    return attached_units_dict[unit_id].getMeasuringStatus()
+
+@app.get("/other_units/{unit_id}/status")
+def get_other_status_by_id(unit_id: int):
+    '''
+    Return other unit status (i.e. {light, current, rtd})
+    '''
+    unit = others_dict[unit_id]
+    if unit.getUnit() == "gizmo":
+        return True # Assume gizmo is always ON
+    else:
+        return others_dict[unit_id].getCrateStatus()
+    
+#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---
+# PUT METHODS
+#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---
+@app.put("/attached_units/{unit_id}/{measuring}/turn-on")
+def turnON_attached_by_id(unit_id: int, measuring: str):
+    '''
+    Turn on measuring from unit connected to module (i.e. light readout from MPOD)
+    '''
+    attached_units_dict[unit_id].powerON(measuring)
+    return {"message" : "Measuring activated"} 
+
+@app.put("/attached_units/{unit_id}/{measuring}/turn-off")
+def turnOFF_attached_by_id(unit_id: int, measuring: str):
+    '''
+    Turn off measuring from unit connected to module (i.e. light readout from MPOD)
+    '''
+    attached_units_dict[unit_id].powerOFF(measuring)
+    return {"message" : "Measuring deactivated"} 
+
+@app.put("/other_units/{unit_id}/turn-on")
+def turnON_other_by_id(unit_id: int):
+    '''
+    Turn on unit NOT connected to module (i.e. MPOD Crate)
+    '''
+    if others_dict[unit_id].getUnit() == "gizmo":
+        return {"message" : "Gizmo always on"}
+    else:
+        others_dict[unit_id].PSU_switch(1)
+        time.sleep(2)
+        return {"message" : "MPOD Crate ON"} 
+    
+@app.put("/other_units/{unit_id}/turn-off")
+def turnON_other_by_id(unit_id: int):
+    '''
+    Turn off unit NOT connected to module (i.e. MPOD Crate)
+    '''
+    if others_dict[unit_id].getUnit() == "gizmo":
+        return {"message" : "Gizmo always on"}
+    else:
+        others_dict[unit_id].PSU_switch(0)
+        time.sleep(2)
+        return {"message" : "MPOD Crate OFF"} 
