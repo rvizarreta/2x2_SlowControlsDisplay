46049969efe420383d974f7c566b339d8c4c510a
ArgonCube <rvizarreta14@gmail.com>
2023-08-06 20:59:23 -0500

    API almost done, continuous monitoring pending

--- MyApp/test_server.py
+++ MyApp/test_server.py
@@ -1,4 +1,4 @@
 import requests
-#print(requests.put("http://127.0.0.1:8000/other_units/1/turn-on").json())
-print(requests.put("http://127.0.0.1:8000/attached_units/0/light/turn-off").json())
+print(requests.put("http://127.0.0.1:8000/other_units/0/turn-off").json())
+#print(requests.put("http://127.0.0.1:8000/attached_units/0/light/turn-off").json())
 
