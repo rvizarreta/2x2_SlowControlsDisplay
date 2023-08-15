5a543c9c7eeae2ac1b68849406f6f0b2fe68ab99
ArgonCube <rvizarreta14@gmail.com>
2023-08-15 17:26:37 -0500

    Other units API connection done

--- MyApp/my-app/src/App.js
+++ MyApp/my-app/src/App.js
@@ -8,32 +8,66 @@ import React, { useState, useEffect } from 'react';
 
 function App() {
   const [othersNames, setOthersNames] = useState([]);
+  const [othersData, setOthersData] = useState([]);
+  const [modulesData, setModulesData] = useState([]);
 
+  // GETTING allothers JSON
   useEffect(() => {
     fetch("http://localhost:8000/allothers")
       .then(response => response.json())
       .then(data => {
+        // Get response JSON
         const others_names = Object.keys(data);
         setOthersNames(others_names)
-        console.log(others_names)
+        // Create othersData based on others_names,
+        const newOthersData = [];
+        for (let i = 0; i < others_names.length; i += 2) {
+          newOthersData.push([
+            //console.log(JSON.stringify(othersNames.length))
+            <Card id={i}
+                  title={others_names[i].toUpperCase()}
+                  on_message={data[others_names[i]]["on_message"]}
+                  off_message={data[others_names[i]]["off_message"]} />,
+            <Card id={i+1}
+                  title={others_names[i + 1].toUpperCase()}
+                  on_message={data[others_names[i+1]]["on_message"]}
+                  off_message={data[others_names[i+1]]["off_message"]} />,
+          ]);
+        
+        }
+        setOthersData(newOthersData);
       });
   }, []);
-  
 
-  const othersData = [
-    [<Card />, <Card />],
-    [<Card />, <Card />],
-  ];
-  const modulesData = [
-    [<ModuleBox />, <ModuleBox />],
-    [<ModuleBox />, <ModuleBox />]
-  ];
+  // GETTING modules JSON
+  useEffect(() => {
+    fetch("http://localhost:8000/allmodules")
+      .then(response => response.json())
+      .then(data => {
+        // Get response JSON
+        const modules_names = Object.keys(data);
+        setOthersNames(modules_names)
+        // Create modulesData based on modules_names,
+        const newModulesData = [];
+        for (let i = 0; i < modules_names.length; i += 2) {
+          const numericPart = modules_names[i].match(/\d+/)[0]
+          const numericValue = parseInt(numericPart, 10)
+          const formattedString = `Module ${numericValue}`;
+          newModulesData.push([
+            <ModuleBox id={i}
+                  title={formattedString}/>,
+            //<ModuleBox id={i+1}
+            //      title={modules_names[i + 1].toUpperCase()}/>,
+          ]);
+        
+        }
+        setModulesData(newModulesData);
+      });
+  }, []);
+
   return (
     <div className="mother_container">
       <Header />
-      {othersNames.map((name, index) => (
-          <span key={index}>{name} gg </span>
-        ))}
       <div className='title-container'>
         <div className='modules_group'>
           <div className='circle'>
--- MyApp/my-app/src/components/card.js
+++ MyApp/my-app/src/components/card.js
@@ -2,6 +2,7 @@ import '../App.css';
 import * as React from 'react';
 import Button from '@mui/material/Button';
 import { createTheme, colors, ThemeProvider } from '@mui/material';
+import { useEffect } from 'react';
 
 // SETTING UP BUTTON THEME
 const theme = createTheme({
@@ -11,33 +12,74 @@ const theme = createTheme({
     },
     secondary:{
       main: colors.red[700]
+    },
+    disabled:{
+      main: colors.red[700]
     }
   }
 })
 
 // COMPONENT FUNCTION
-function Card() {
+const Card = ({ id, title, on_message, off_message }) => {
+
   //#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---
   //# BUTTON STATUS CONFIGURATION
   //#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---
+
+  const [status, setStatus] = React.useState(false); // Initialize status
   const [clicked, setClicked] = React.useState(false);
   const handleClick = () => {
     setClicked((prevClicked) => !prevClicked);
+    const endpoint = clicked ? `http://localhost:8000/other_units/${id}/turn-on` : `http://localhost:8000/other_units/${id}/turn-off`;
+    fetch(endpoint, {method: "PUT"})
+    .then(response => response.json())
+    }
+  
+  const fetchStatus = () => {
+    // Getting status
+    fetch(`http://localhost:8000/other_units/${id}/status`, {cache: 'no-cache'})
+    .then(response => response.json())
+    .then(state => {
+      const parsedStatus = Boolean(state);
+      setStatus(!parsedStatus);
+      setClicked(!parsedStatus);
+    })
   };
+
+  // Fetch the status initially and then set up polling
+  useEffect(() => {
+    fetchStatus(); // Fetch initial status
+    // Set up polling every 10 seconds
+    const intervalId = setInterval(fetchStatus, 10000);
+    // Clean up the interval when the component unmounts
+    return () => clearInterval(intervalId);
+  }, [id]);
+
+  const buttonStyle = {
+    width: 82,
+    height: 30,
+    borderRadius: 0,
+  };
+
+  if (off_message === "Disabled") {
+    buttonStyle.color = "disabled";
+  }
+
   //#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---
   //# RETURN CARD
   //#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---
     return (
       <div className={clicked ? 'card-off' : 'card-on'}>
-        <div style={{display : 'flex'}}>
-          <h2 className="card-title">MPOD Crate</h2>
-          <div style={{marginTop : '8px', paddingLeft : '40px'}}>
+        <div style={{display : 'flex', justifyContent : 'space-between'}}>
+          <div className="card-title">{title}</div>
+          <div style={{marginTop : '8px', paddingRight : '8px'}}>
             <ThemeProvider theme={theme}>
               <div>
                 <Button color={clicked ? 'secondary' : 'primary'}
                         variant="contained"
-                        style={{width: 82, height: 30, borderRadius: 0}}
-                        onClick={handleClick}>
+                        style={buttonStyle}
+                        onClick={handleClick}
+                        disabled={off_message === "Disabled"}>
                         {clicked ? 'OFF' : 'ON'}
                 </Button>
               </div>
@@ -45,7 +87,7 @@ function Card() {
           </div>
         </div>
         <p className={clicked ? 'card-text-off' : 'card-text-on'}>
-        {clicked ? 'MPOD crate is off. Nothing is supplied to the modules.' : 'MPOD crate is on. Turning it off will affect all modules.'}
+        {clicked ? off_message : on_message}
         </p>
       </div>
     );
