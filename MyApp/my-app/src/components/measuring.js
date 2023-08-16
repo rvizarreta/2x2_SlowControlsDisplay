377c2f4143e147eb03f41044fbff6d3ecdb88ae0
ArgonCube <rvizarreta14@gmail.com>
2023-08-16 01:27:26 -0500

    Version 1.0 is ready!

--- MyApp/my-app/src/components/measuring.js
+++ MyApp/my-app/src/components/measuring.js
@@ -2,6 +2,7 @@ import '../App.css';
 import * as React from 'react';
 import Button from '@mui/material/Button';
 import { createTheme, colors, ThemeProvider } from '@mui/material';
+import { useEffect } from 'react';
 
 // SETTING UP BUTTON THEME
 const theme = createTheme({
@@ -15,27 +16,43 @@ const theme = createTheme({
     }
   })
 
-function Measuring(){
+const Measuring= ({ id, title, status, button_status }) => {
 
   //#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---
   //# BUTTON STATUS CONFIGURATION
   //#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---
-  const [clicked, setClicked] = React.useState(false);
+  console.log(JSON.stringify(status))
+  const [clicked, setClicked] = React.useState(!status);
+
+  useEffect(() => {
+    setClicked(!status);
+  }, [status]);
+
   const handleClick = () => {
     setClicked((prevClicked) => !prevClicked);
-  };
+    const endpoint = clicked ? `http://localhost:8000/attached_units/${id}/${title}/turn-on` : `http://localhost:8000/attached_units/${id}/${title}/turn-off`;
+    fetch(endpoint, {method: "PUT"})
+    .then(response => response.json())
+    }
+
+  // Setting up readout name
+  const firstWord = title;
+  const capitalizedFirstLetter = firstWord.charAt(0).toUpperCase();
+  const restOfWord = firstWord.slice(1);
+  const newTitle = `${capitalizedFirstLetter}${restOfWord} readout`;
 
   return (
       <div className={clicked ? 'measuring-row-off' : 'measuring-row-on'}>
         <div className='measuring'>
-          Light Readout
+          {newTitle}
         </div>
         <ThemeProvider theme={theme}>
           <div style={{paddingLeft : '10px'}}> 
             <Button color={clicked ? 'secondary' : 'primary'}
                     variant="contained"
                     style={{width: 82, height: 30, borderRadius: 0}}
-                    onClick={handleClick}>
+                    onClick={handleClick}
+                    disabled={!button_status}>
                     {clicked ? 'OFF' : 'ON'}
             </Button>
           </div>
